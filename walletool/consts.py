07cfe6f47538f48a2d8cd12cd20e54173dd8c3da
Jeremy <jeremy-wilder@users.noreply.github.com>
2021-05-14 23:53:33 -0500

    Update consts.py

--- walletool/consts.py
+++ walletool/consts.py
@@ -7,3 +7,13 @@ addrtypes = {
     'dogecoin': 30,
     'dash': 76,
 }
+
+def show_private(v):
+    if isinstance(v, bool):
+       return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
